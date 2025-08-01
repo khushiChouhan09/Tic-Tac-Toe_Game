38e5c9b470f873d90421185b3abedd0974a9dc7c
Tejbhan Sahu <its.khushi.chouhan@gmail.com>
2025-08-01 19:25:43 +0530

    initial code for tic-tac-toe_game

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TicTacTeo from './Components/TicTacTeo/TicTacTeo';
+
+function App() {
+  return (
+    <div >
+        <TicTacTeo/>
+    </div>
+  );
+}
+
+export default App;
